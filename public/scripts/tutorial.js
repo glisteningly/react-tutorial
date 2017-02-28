684b2b80eb57c661c4e81df1e690f53a9ac95e3c
tyf <arexsm@126.com>
2017-02-28 17:50:17 +0800

    加入comment

--- public/scripts/tutorial.js
+++ public/scripts/tutorial.js
@@ -1,3 +1,20 @@
+class Comment extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
 class CommentList extends React.Component {
   constructor(props) {
     super(props);
